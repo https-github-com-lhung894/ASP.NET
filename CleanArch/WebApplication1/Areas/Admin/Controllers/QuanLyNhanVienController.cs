e7edf3dae80a11e4fce4a37a2b45020404511d51
AlE-dip <71812422+AlE-dip@users.noreply.github.com>
2020-11-18 15:37:22 +0700

    Them giao dien vao app

---
+++ CleanArch/WebApplication1/Areas/Admin/Controllers/QuanLyNhanVienController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("QuanLyNhanVien")]
+    public class QuanLyNhanVienController : Controller
+    {
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
