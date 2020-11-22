d676a08249f0d7ac9da16b4c97ed67293af074e1
conglapp2606 <63706968+conglapp2606@users.noreply.github.com>
2020-11-22 09:48:21 +0700

    Update BangLuong Controller

---
+++ CleanArch/WebApplication1/Areas/Admin/Controllers/BangLuongController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("BangLuong")]
+    public class BangLuongController : Controller
+    {
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }        
+}
